dc841ec027e9825f9400a3bf2b035abe73a5ff9f
cocktailpeanut <cocktailpeanuts@proton.me>
2024-11-25 16:31:59 -0500

    update

--- install.js
+++ install.js
@@ -5,7 +5,7 @@ module.exports = {
       method: "shell.run",
       params: {
         message: [
-          "git clone https://huggingface.co/spaces/cocktailpeanut/OminiControl app",
+          "git clone https://github.com/cocktailpeanut/OminiControl app",
         ]
       }
     },
@@ -28,16 +28,16 @@ module.exports = {
         venv: "env",                // Edit this to customize the venv folder path
         path: "app",                // Edit this to customize the path to start the shell from
         message: [
-          "pip install gradio devicetorch",
+          "pip install gradio",
           "pip install -r requirements.txt"
         ]
       }
     },
-    {
-      method: "fs.link",
-      params: {
-        venv: "app/env"
-      }
-    }
+//    {
+//      method: "fs.link",
+//      params: {
+//        venv: "app/env"
+//      }
+//    }
   ]
 }
