99794da190e5cd62ee983fd0720be6facace2007
cocktailpeanut <cocktailpeanuts@proton.me>
2024-11-27 03:58:12 -0500

    update

--- torch.js
+++ torch.js
@@ -7,7 +7,7 @@ module.exports = {
       "params": {
         "venv": "{{args && args.venv ? args.venv : null}}",
         "path": "{{args && args.path ? args.path : '.'}}",
-        "message": "pip install torch==2.5.1 torchvision==0.20.1 torchaudio==2.5.1 --index-url https://download.pytorch.org/whl/cu121"
+        "message": "pip install --pre torch torchvision torchaudio {{args && args.xformers ? 'xformers' : ''}} --index-url https://download.pytorch.org/whl/nightly/cu121"
 
       }
     },
